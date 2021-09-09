64e40b08c636270f08211d5e5b84ac84be4776f9
prince-wonhyo <78794471+prince-wonhyo@users.noreply.github.com>
2021-09-10 01:35:34 +0900

    Add header to Home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,6 +7,21 @@ export default class Home extends Component {
   render() {
     return (
       <div className="page-container">
+        <header className="center">
+          <img
+            className="profile-photo"
+            width="50%"
+            height="auto"
+            style={{ maxHeight: "344px", maxWidth: "344px" }}
+            src="images/profile_pic.jpg"
+            alt="Prince's Photo"
+          />
+          <h1 className="welcome-title">
+            <span className="welcome">Welcome</span> <br /> to the portfolio of
+            the world's most <s>resilient</s> rebellious beginner coder
+            <span class="cursor">_</span>
+          </h1>
+        </header>
       </div>
     );
   }
