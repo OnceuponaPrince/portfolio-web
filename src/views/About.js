b871aed1331781f6288fb2fa0f9fbc015be5e6bf
prince-wonhyo <78794471+prince-wonhyo@users.noreply.github.com>
2021-09-10 17:05:56 +0900

    Create About view

---
+++ src/views/About.js
@@ -0,0 +1,12 @@
+import {Nav, Footer} from "./views/Home.js";
+import React from "react";
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
