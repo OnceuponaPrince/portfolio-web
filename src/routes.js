7a4a22ccd5617435dd5b00f672910c5980171f06
prince-wonhyo <78794471+prince-wonhyo@users.noreply.github.com>
2021-09-10 18:22:12 +0900

    Updated links to all pages and structured site hierarchy

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 
-import Home from "./views/Home.js";
+import About from "./views/About.js";
+import CV from "./views/CV.js";
+import {Home} from "./views/Home.js";
+import Notes from "./views/Notes.js";
 import React from "react";
+import Study from "./views/Study.js";
 
 export const Routes = () => {
   return (
@@ -11,6 +15,10 @@ export const Routes = () => {
         <Route exact path="/Home">
           <Redirect to="/" />
         </Route>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/notes" component={Notes} />
+        <Route exact path="/cv" component={CV} />
+        <Route exact path="/study" component={Study} />
       </Switch>
     </>
   );
