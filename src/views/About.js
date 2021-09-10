9032d411b628108b3939199bb5b536c15ad06f4a
prince-wonhyo <78794471+prince-wonhyo@users.noreply.github.com>
2021-09-10 18:03:49 +0900

    Styled and structured About page

--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,14 @@
-import {Nav, Footer} from "./views/Home.js";
-import React from "react";
+import {Footer, Nav} from "./Home.js";
+import React, {Component} from "react";
 
 export default class About extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="page-container container-margin">
+               <Nav />
+                <h1 className="page-headings center">About Me</h1>
+                <p className="page-content plain-text center">Something... something... random fact about me. Now we all laugh as we run down to the stream to skip stones. "What a lovely day this is!"</p>
+               <Footer />
             </div>
         )
     }
