7c6fd0552d0b6f3e823fbcee6faf7c98be2c45d2
prince-wonhyo <78794471+prince-wonhyo@users.noreply.github.com>
2021-09-12 06:34:55 +0900

    Add loop function for dynamic note creation

--- src/views/Notes.js
+++ src/views/Notes.js
@@ -1,39 +1,69 @@
+import 'react-dom/server';
+
 import {Footer, Nav} from "./Home.js";
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
 import TimeAgo from 'react-timeago';
+import buildFormatter from 'react-timeago/lib/formatters/buildFormatter';
 
-export default class Notes extends Component {
+export class Notes extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            // JSONs containing each entry and their metadata.
-            files: [
-                "data/autism_adhd_and_other_superpowers.txt",
-                "data/terminal_and_git.txt",
-                "data/writing_code_comedy.txt",
-                "data/i_failed_a_resit.txt"
-            ]
-        }
     }
-    
-    render() {
-        let fileArray = this.state.files;
-        let entries = [];
-        /* 
-        fileArray.map((value) => {
-            entries.push((<Card title=value.title date=value.date id=value.id content=value.content />));
+    promiseCreator = (value) => {
+        let promise = new Promise((resolve, reject) => {
+            value ? resolve(value) : reject(value);
         });
-        */
+        return promise;
+    }
+
+    errorString = () => {
+        return (<p className="plain-text">An error occurred! Refresh the page or try again later.</p>);
+    }
+    render() {
         return (
-            <div className="page-container container-margin">
+            <div className="page-container notes container-margin">
                 <Nav />
                 <h1 className="page-heading center">Notes</h1>
-                <div className="card-column center">{entries}</div>             
+                <CardColumn promiseCreator={this.promiseCreator}
+                            errorString={this.errorString} />
                 <Footer />
             </div>
         )
     }
+}
+
+const CardColumn = (props) => {
+    const [ready, setReady] = useState(false);
+    const [status, setStatus] = useState("Loading entries...");
+    const [cardColumn, setCardColumn] = useState("");
+    useEffect(() => {
+        // Array of files containing each entry and their metadata.
+        let noteEntries = require("../data/noteEntries.json");
+        let cardArray = [];
+        console.log(noteEntries);
+        noteEntries.map((value, index) => {
+            //Create Card component for each entry.
+            cardArray.push(<Card key={index} id={"entry#" + {index}} title={value.title} date={value.date} content={value.content} />);
+        });
+        if (cardArray.length === noteEntries.length) {
+            //Only show all cards when fully loaded
+            setCardColumn(cardArray);
+            setReady(true);
+        } else {
+            //
+            console.log(cardArray.length);
+            let x = cardArray.length;
+            let y = noteEntries.length;
+            setStatus("Loaded " + x + " out of " + y + " entries...");
+        }
+    }, [status]);
+    return (
+
+        <div className="cards-column center">
+           {ready ? cardColumn : status}
+        </div>
+    );
 }
 
 class Card extends Component {
@@ -46,6 +76,13 @@ class Card extends Component {
         }
     }
 
+    contentHandler = (text) => {
+        text = "<p>" + text.replace(/\n/g, "<br />") + "</p>";
+        let HTMLDoc = new DOMParser().parseFromString(text.replace(/\\\"/g,'"'), "text/html");
+        text = HTMLDoc.body.innerHTML;
+        console.log(text);
+        return (text);
+    }
 
     expandIcon = () => {
         // Returns a caret pointing downwards, to be used as the icon for the button that expands each entry.
@@ -56,40 +93,39 @@ class Card extends Component {
          // Returns a caret pointing right, to be used as the icon for the button that collapse each entry.
         return (<i className="bi bi-caret-right-fill"></i>);
     }
-    
-    changeStates = (event) => {
-        if (!this.state.expand) {
-            this.setState({
-                expand: true,
-                buttonTitle: "Collapse entry",
-                buttonLabel: "Collapse"
-            })
-        } else {
-            this.setState({
-                expand: false,
-                buttonTitle: "Expand entry",
-                buttonLabel: "Expand"
-            })
-        }
-    }
-
     render() {
+        let content = this.contentHandler(this.props.content);
+        let object = [content];
         let buttonIcon = this.expandIcon();
-        this.state.expand ? button = this.collapseIcon() : button = this.expandIcon();
+        this.state.expand ? buttonIcon = this.collapseIcon() : buttonIcon = this.expandIcon();
         return (
-            <div className="card" aria-role="document">
-                        <span class="details" aria-label="Entry information">
+            <div key={this.props.id} className="card" role="document">
+                        <span className="details" aria-label="Entry information">
                             <h2>{this.props.title}</h2>
                             <h3><TimeAgo date={this.props.date} /></h3>
                             <button className="center" 
-                                    onClick={this.changeStates()}
+                                    onClick={(event) => {
+                                        if (!this.state.expand) {
+                                            this.setState({
+                                                expand: true,
+                                                buttonTitle: "Collapse entry",
+                                                buttonLabel: "Collapse"
+                                            })
+                                        } else {
+                                            this.setState({
+                                                expand: false,
+                                                buttonTitle: "Expand entry",
+                                                buttonLabel: "Expand"
+                                            })
+                                        }
+                                    }}
                                     title={this.state.buttonTitle}
                                     aria-label={this.state.buttonLabel}
                                     aria-expanded={this.state.expand}>{buttonIcon}</button>
                         </span>
                         
-                        <article id={this.props.id} className={this.state.expand ? "plain-text" : "invisible"}>
-                                <p>{this.props.content}</p>
+                        <article className={this.state.expand ? "plain-text" : "invisible"}>
+                            <div dangerouslySetInnerHTML={{__html: content }}></div>
                         </article>
                     </div>
         );
