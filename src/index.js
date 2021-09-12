d5cb02d31138ada206b020dbfcc40fcb201cf63a
prince-wonhyo <78794471+prince-wonhyo@users.noreply.github.com>
2021-09-13 04:18:06 +0900

    Add Splash Screen

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./routes";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <Router>
-    <Routes />
-  </Router>,
-  document.getElementById("root")
-);
+function load() {
+  // This function will allow other functions to execute only when this is called successfully.
+  return new Promise(resolve => {
+    ReactDOM.render(<Router>
+      <Routes />
+    </Router>, document.getElementById('root'), resolve);
+  })
+}
+
+load().then(() => {
+  // When the render is successful, the splash screen will be removed.
+  document.body.removeChild(  document.getElementById("splashScreen"));
+  if(document.getElementById("root").className === "hidden") {
+    document.getElementById("root").className =
+    document.getElementById("root").className.replace
+        ( /(?:^|\s)hidden(?!\S)/g , '' );
+  }
+});
+
+//Status log stack dump
 reportWebVitals();
